4ff411d5e2c1542d9c9e6502d2a55a9aa605017a
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-10 11:45:16 +0000

    Deploying to gh-pages from @ yusd24/new_webpage@2b8ecdab73a4b82deeccb1059a5a94c3975287ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%73%75%6E%67%6A%75@%67%73.%6E%63%6B%75.%65%64%75.%74%77", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=", "_blank");
         },
       },{
         id: 'social-custom_social',
