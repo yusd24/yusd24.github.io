8f1dc2536bf866ed70833b956e26a630fdff95cf
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-12 09:49:53 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io.tem@d5253a1606e0a8f3fd33e25ab4460f6f984bb492 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yusd24/new_webpage",
+    "data-repo": "yusd24/yusd24.github.io.tem",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
