ab62c4ef321238e1e2bd2a3a9002fbc906595000
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-12 22:57:27 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@038ead544231b165a11c1dc17ef8f2b5cb46cb69 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "Publications by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
