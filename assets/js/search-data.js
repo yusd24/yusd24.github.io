7cc68fa42cb3471528a65105e21c35c422921c8b
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-19 08:30:51 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@13de884d02e63b1a4e82cd5f44ea8be4925c3584 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,17 +436,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/images/";
           
         },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
