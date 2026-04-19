cb921c2d4037bb4b41727c728d01ff03ea80a1e3
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-19 08:33:07 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@c1bd48a0e4b0e50b6fa3cfc3ed83f377520e7127 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,17 +84,6 @@ ninja.data = [{
             window.location.href = "/blog/2026/latex-installation/";
           
         },
-      },{id: "post-a-post-with-plotly-js",
-        
-          title: "a post with plotly.js",
-        
-        description: "this is what included plotly.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/plotly/";
-          
-        },
       },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
