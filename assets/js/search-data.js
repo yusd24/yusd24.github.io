5ea96e07b37ac0cf634bad8d1e6ef04845e93745
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-19 08:31:41 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@b2bbd460f1c202d540d6d3151c54a6fefeed06dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,17 +425,6 @@ ninja.data = [{
             window.location.href = "/blog/2015/code/";
           
         },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
