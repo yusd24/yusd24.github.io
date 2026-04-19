87b2b8b10fa3cd2b521ac71da4c04037a28dca3e
yusd24 <69486535+yusd24@users.noreply.github.com>
2026-04-19 08:33:56 +0000

    Deploying to gh-pages from @ yusd24/yusd24.github.io@52f62424f6a36838977984ff6b39ec2ae9c32043 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,17 +95,6 @@ ninja.data = [{
             window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
           
         },
-      },{id: "post-a-post-with-typograms",
-        
-          title: "a post with typograms",
-        
-        description: "this is what included typograms code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/typograms/";
-          
-        },
       },{id: "post-a-post-that-can-be-cited",
         
           title: "a post that can be cited",
